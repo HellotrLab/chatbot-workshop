062febebbbc6bfd1b46859306fed691cb555a19c
HellotrLab <phuongtrangvu05@gmail.com>
2025-02-05 10:41:21 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.gemini import GeminiEmbedding
 
 
-st.set_page_config(page_title="Chat with an expert on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with an expert on the works of A Dollhouse", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Chat with an expert on the works of Rabindranath Tagore")
 
 
@@ -12,7 +12,7 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Rabindranath Tagore!!",
+            "content": "Ask me a question about A Dollhouse!!",
         }
     ]
 
@@ -27,10 +27,10 @@ def load_data():
     
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
-        temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
+        temperature=0.2,
+        system_prompt="""You are an expert on the work of A Dollhouse.
+        Answer the question using the provided documents, which contain relevant excerpts from the work of A Dollhouse.
+        The context for all questions is the work of A Dollhouse. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
         Respond using a florid but direct tone, typical of an early modernist writer.
         Keep your answers under 100 words.""",
         api_key = st.secrets.google_gemini_key,
